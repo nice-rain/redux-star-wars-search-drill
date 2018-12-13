16b50dd393a1eb755dfd6130bf2144704e956f6d
Glitch (accessible-front) <none>
2018-12-13 17:36:58 +0000

    💁🚂 Updated with Glitch

---
+++ src/reducers.js
@@ -0,0 +1,30 @@
+import {
+    SEARCH_CHARACTERS_REQUEST,
+    SEARCH_CHARACTERS_SUCCESS,
+    SEARCH_CHARACTERS_ERROR
+} from './actions';
+
+const initialState = {
+    characters: [],
+    loading: false,
+    error: null
+};
+
+export function characterReducer(state=initialState, action) {
+    // Handle these sync actions
+    if (action.type === SEARCH_CHARACTERS_REQUEST) {
+      return Object.assign({}, state, {loading:true});
+    }
+    else if (action.type === SEARCH_CHARACTERS_SUCCESS) {
+      return Object.assign({}, state, {
+        characters: action.characters,
+        loading:false,
+        error:null});
+    }
+    else if (action.type === SEARCH_CHARACTERS_ERROR) {
+      return Object.assign({}, state, {
+        error:action.error,
+        loading:false});
+    }
+    return state;
+}
